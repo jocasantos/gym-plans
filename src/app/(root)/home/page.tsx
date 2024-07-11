742d32574c2cc2d8cf360a0cc1aa1a554d1f626e
giga <tacuara10@gmail.com>
2024-07-11 16:33:44 +0100

    refactor muscleForm and HomePage

--- src/app/(root)/home/page.tsx
+++ src/app/(root)/home/page.tsx
@@ -2,16 +2,19 @@
 
 import { pageHeader } from "@/app/constants";
 import Header from "@/components/shared/Header";
-import MuscleForm from "@/components/shared/MuscleForm";
+import MuscleForm, { MuscleFormProps } from "@/components/shared/MuscleForm";
 import ShowPage from "@/components/shared/ShowPage";
 import React, { useState } from "react";
 
 const DashboardPage = () => {
   // State to control whether to show the form or the ShowPage component
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [formData, setFormData] = useState(null);
 
   // Callback function to be called after form submission
-  const handleFormSubmit = () => {
+  const handleFormSubmit = (values: any) => {
+    console.log("Form Submitted", values);
+    setFormData(values);
     setIsSubmitted(true); // Update state to show the ShowPage component
   };
 
--- src/components/shared/MuscleForm.tsx
+++ src/components/shared/MuscleForm.tsx
@@ -71,22 +71,15 @@ const formSchema = z
   });
 
 // 1. Define an interface for the MuscleForm props
-interface MuscleFormProps {
+export interface MuscleFormProps {
   onSubmit: (values: z.infer<typeof formSchema>) => void; // Define the type of the onSubmit prop
 }
 
-const MuscleForm = () => {
+const MuscleForm: React.FC<MuscleFormProps> = ({ onSubmit }) => {
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
   });
-
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
-  }
 
   return (
     <Form {...form}>
