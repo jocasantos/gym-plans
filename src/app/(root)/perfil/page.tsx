2ab2e371d6f29384bdc59d3e160554970c5ca0c5
giga <tacuara10@gmail.com>
2024-07-25 12:08:54 +0100

    chore: Refactor profile page and add credit balance and simulations count

--- src/app/(root)/perfil/page.tsx
+++ src/app/(root)/perfil/page.tsx
@@ -1,18 +1,55 @@
-import ShowPage from "@/components/shared/ShowPage";
-import SimulText from "@/components/shared/SimulText";
-import { getMuscleMass } from "@/lib/utils";
-import React from "react";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 
-const ProfilPage = () => {
-  const test = {
-    height: 200,
-    age_start: 14,
-    genre: "feminino",
-  };
+import Header from "@/components/shared/Header";
+import { getUserById } from "@/lib/actions/user.actions";
+import { getAllSimulations } from "@/lib/actions/simul.actions";
+import { auth } from "@clerk/nextjs/server";
+
+const Profile = async ({ searchParams }: SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+  const simulacoes = await getAllSimulations();
 
   return (
-    <div className="flex-col flex gap-8 items-center lg:flex lg:flex-row"></div>
+    <>
+      <Header title="Profile" />
+
+      <section className="profile">
+        <div className="profile-balance">
+          <p className="p-14-medium md:p-16-medium">CRÉDITOS DISPONÍVEIS</p>
+          <div className="mt-4 flex items-center gap-4">
+            <Image
+              src="/assets/icons/coins.svg"
+              alt="coins"
+              width={50}
+              height={50}
+              className="size-9 md:size-12"
+            />
+            <h2 className="h2-bold text-dark-600">{user.creditBalance}</h2>
+          </div>
+        </div>
+
+        <div className="profile-image-manipulation">
+          <p className="p-14-medium md:p-16-medium">SIMULAÇÕES REALIZADAS</p>
+          <div className="mt-4 flex items-center gap-4">
+            <Image
+              src="/assets/icons/photo.svg"
+              alt="coins"
+              width={50}
+              height={50}
+              className="size-9 md:size-12"
+            />
+            <h2 className="h2-bold text-dark-600">{simulacoes?.length}</h2>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
-export default ProfilPage;
+export default Profile;
